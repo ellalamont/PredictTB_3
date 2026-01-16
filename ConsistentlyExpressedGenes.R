da2f04178a697d3a0a99a7b5416ba63e47854c27
ellalamont <elamont@uw.edu>
2026-01-16 12:35:10 -0800

    Wrote what the numbers are

--- ConsistentlyExpressedGenes.R
+++ ConsistentlyExpressedGenes.R
@@ -25,13 +25,17 @@ W0_RawReads <- All_RawReads_f %>%
   select(all_of(W0SputumSamplesAll_pipeSummary$SampleID2))
 
 # What is 80% of the samples
-min_samples <- ceiling(0.8 * ncol(W0_RawReads)) # 52 is 80%
+min_samples <- ceiling(0.8 * ncol(W0_RawReads)) 
+# 52 is 80% for all
+# 38 is 80% for >1e6 reads
 
 # Filter to get just the genes that have >=100 reads in at least 80% of the samples
 genes_passing <- W0_RawReads %>%
   mutate(nSamples_w100reads = rowSums(across(everything(), ~ .x >= 100))) %>%
   filter(nSamples_w100reads >= min_samples)
-nrow(genes_passing) # 155 genes 
+nrow(genes_passing) 
+# 155 genes for all
+# 2,351 genes for >1e6 read samples
 
 # Filter to get just the genes that have >=10 reads in at least 80% of the samples
 # genes_passing <- W0_RawReads %>%
@@ -56,19 +60,23 @@ W2_RawReads <- All_RawReads_f %>%
   select(all_of(W2SputumSamplesAll_pipeSummary$SampleID2))
 
 # What is 80% of the samples
-min_samples <- ceiling(0.8 * ncol(W2_RawReads)) # 45 is 80%
+min_samples <- ceiling(0.8 * ncol(W2_RawReads)) 
+# 45 is 80% for all
+# 24 is 80% for >1e6 reads
 
 # Filter to get just the genes that have >=100 reads in at least 80% of the samples
 genes_passing <- W2_RawReads %>%
   mutate(nSamples_w100reads = rowSums(across(everything(), ~ .x >= 100))) %>%
   filter(nSamples_w100reads >= min_samples)
-nrow(genes_passing) # 15 genes 
+nrow(genes_passing) 
+# 15 genes for all
+# 103 genes for >1e6 read samples
 
 # Filter to get just the genes that have >=10 reads in at least 80% of the samples
-genes_passing <- W2_RawReads %>%
-  mutate(nSamples_w10reads = rowSums(across(everything(), ~ .x >= 10))) %>%
-  filter(nSamples_w10reads >= min_samples)
-nrow(genes_passing) # 227 genes
+# genes_passing <- W2_RawReads %>%
+#   mutate(nSamples_w10reads = rowSums(across(everything(), ~ .x >= 10))) %>%
+#   filter(nSamples_w10reads >= min_samples)
+# nrow(genes_passing) # 227 genes
 
 
 
